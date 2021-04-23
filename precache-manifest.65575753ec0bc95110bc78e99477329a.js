d456ec925f8c925f184d4afd6652382d3ed79ec9
lsancho <lsanchoaragon@outlook.com>
2021-04-23 15:19:08 +0200

    Updates

---
+++ precache-manifest.65575753ec0bc95110bc78e99477329a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0cd2b8e6760080b7a72bc3ccde2cf3",
+    "url": "/LSanchoES/Heroes-app/index.html"
+  },
+  {
+    "revision": "07ac8ee8bfda3431808f",
+    "url": "/LSanchoES/Heroes-app/static/js/2.b841f0f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LSanchoES/Heroes-app/static/js/2.b841f0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cb705a650d2ca5bf57",
+    "url": "/LSanchoES/Heroes-app/static/js/main.06b28f39.chunk.js"
+  },
+  {
+    "revision": "8214d78cd81b6f84a37d",
+    "url": "/LSanchoES/Heroes-app/static/js/runtime-main.d4a7c83b.js"
+  },
+  {
+    "revision": "a179d8711d59015e5d3d53637ad9fce7",
+    "url": "/LSanchoES/Heroes-app/static/media/dc-arrow.a179d871.jpg"
+  },
+  {
+    "revision": "8efedfa7bf353070fb1dc56bc36a8bd3",
+    "url": "/LSanchoES/Heroes-app/static/media/dc-batman.8efedfa7.jpg"
+  },
+  {
+    "revision": "5d203be297ee7aee6df388f48effc2d2",
+    "url": "/LSanchoES/Heroes-app/static/media/dc-black.5d203be2.jpg"
+  },
+  {
+    "revision": "871a00dda57a453a698369e1687db44e",
+    "url": "/LSanchoES/Heroes-app/static/media/dc-blue.871a00dd.jpg"
+  },
+  {
+    "revision": "d22cd5d0cdbb8e6cb64ea844fd1082fa",
+    "url": "/LSanchoES/Heroes-app/static/media/dc-flash.d22cd5d0.jpg"
+  },
+  {
+    "revision": "dae0db0a19c068035a119a4cf07bfd39",
+    "url": "/LSanchoES/Heroes-app/static/media/dc-green.dae0db0a.jpg"
+  },
+  {
+    "revision": "9629381c9ae8fe6a5c9ab23308cd76f0",
+    "url": "/LSanchoES/Heroes-app/static/media/dc-martian.9629381c.jpg"
+  },
+  {
+    "revision": "3b3b7dd1a4716a9f8eec3c13c0611726",
+    "url": "/LSanchoES/Heroes-app/static/media/dc-robin.3b3b7dd1.jpg"
+  },
+  {
+    "revision": "ad88dd859f1b6cb05d235edfdf7d2da9",
+    "url": "/LSanchoES/Heroes-app/static/media/dc-superman.ad88dd85.jpg"
+  },
+  {
+    "revision": "b01172005be3eba0ddbe50bd07d3ece8",
+    "url": "/LSanchoES/Heroes-app/static/media/dc-wonder.b0117200.jpg"
+  },
+  {
+    "revision": "71c5a519e43cab5350b5a5dbb45f4ece",
+    "url": "/LSanchoES/Heroes-app/static/media/marvel-captain.71c5a519.jpg"
+  },
+  {
+    "revision": "017af60c9d646181629f0887c02e8ef0",
+    "url": "/LSanchoES/Heroes-app/static/media/marvel-cyclops.017af60c.jpg"
+  },
+  {
+    "revision": "3d4c3df826dce8f0a85e8e49bd3fb204",
+    "url": "/LSanchoES/Heroes-app/static/media/marvel-daredevil.3d4c3df8.jpg"
+  },
+  {
+    "revision": "df41e28ce57042715584e972ea53e5a2",
+    "url": "/LSanchoES/Heroes-app/static/media/marvel-hawkeye.df41e28c.jpg"
+  },
+  {
+    "revision": "8f5cd9397468b7b508ffa33b00c5014e",
+    "url": "/LSanchoES/Heroes-app/static/media/marvel-hulk.8f5cd939.jpg"
+  },
+  {
+    "revision": "8f307eef75cd45b3e7076ece1b368e5f",
+    "url": "/LSanchoES/Heroes-app/static/media/marvel-iron.8f307eef.jpg"
+  },
+  {
+    "revision": "c055e4a00bff140e8faf40eee0701eaa",
+    "url": "/LSanchoES/Heroes-app/static/media/marvel-silver.c055e4a0.jpg"
+  },
+  {
+    "revision": "9967f816d7adb4c674f99d230e9472c7",
+    "url": "/LSanchoES/Heroes-app/static/media/marvel-spider.9967f816.jpg"
+  },
+  {
+    "revision": "9f1b74e51e318f14c533fdb3b03a7421",
+    "url": "/LSanchoES/Heroes-app/static/media/marvel-thor.9f1b74e5.jpg"
+  },
+  {
+    "revision": "a1a41d9adf2f16f1220723a70ce20215",
+    "url": "/LSanchoES/Heroes-app/static/media/marvel-wolverine.a1a41d9a.jpg"
+  }
+]);
